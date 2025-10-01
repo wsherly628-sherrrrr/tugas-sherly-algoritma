84d89628df75ca6791257b76e14776fa39ebf5d9
wsherly628-sherrrrr <wsherly628@gmail.com>
2025-10-01 11:05:20 +0700

    Add files via upload

---
+++ tugas 2.py
@@ -0,0 +1,30 @@
+bu_kota="jakarta"
+print(ibu_kota)
+h=1; j=2; k=h+j
+print(k)
+q=10
+if q>5:
+    print('nilai a lebih dari 5')
+a:1; b:2; c:"mantap"
+a,b,c=1,2,"mantap"
+d:10; e:10; f:10
+d=e=f=10
+print('d',d)
+print('f',f)
+print('e',e)
+print('a',a)
+print('b',b)
+print('c',c)
+nama_depan="sherly"
+nama_belakang="wulandari"
+nama_lengkap=nama_depan+nama_belakang
+usia="18"
+print(nama_lengkap); print('usia',usia)
+nama: sherly wulandari
+usia: 18
+belum_menikah: flase
+panjang = 5
+lebar = 10.5
+luas - panjang = lebar
+saya_anak_indonesia- True
+saya_adalah_mahasiswa = false
